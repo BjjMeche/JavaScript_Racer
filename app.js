a83f3c55bd008eed29e97213f0bc9beaba0295b0
bjjmeche <fhshaw7@gmail.com>
2015-03-03 15:42:05 -0800

    Implement Javascript Racer in OOJS

--- app.js
+++ app.js
@@ -1,76 +1,81 @@
-game = {
-  racetrackAdditionalLength :  null,
-  playerOneTrack : $("#player1_strip"),
-  playerTwoTrack : $("#player2_strip"),
+players={
   player1:"Human 1",
   player2:"Human 2",
+
+  assignPlayers: function(){
+    players.player1 = prompt("Player 1 Name")
+    players.player2 = prompt("Player 2 Name")
+  },
+},
+
+racetrack={
   racetrackLength : 4,
   carLocation:[],
-  resetGame: function(){
-    location.reload()
-  },
+  playerOneTrack: $("#player1_strip"),
+  playerTwoTrack: $("#player2_strip"),
+
   additionalTrackLength: function(){
     stringLength = $("#racetrackAddLength")[0].value
     addLength = parseInt(stringLength)
     if (stringLength === ""){
       addLength = 0
     }
-    game.racetrackLength += addLength
-    game.extendTrak(addLength)
+    racetrack.racetrackLength += addLength
+    racetrack.extendTrak(addLength)
   },
 
   extendTrak: function(additionalTrackLength){
     var addTrackLength = additionalTrackLength
     for(var i=0; i<addTrackLength; i++){
-      game.playerOneTrack.append("<td></td>")
-      game.playerTwoTrack.append("<td></td>")
-    }
-  },
-
-  assignPlayers: function(){
-    player1 = prompt("Player 1 Name")
-    player2 = prompt("Player 2 Name")
-  },
-
-  jsRacerGame: function(e){
-    if(e.charCode == 97){
-      carLocation = game.carCurrentLocation(game.playerOneTrack)
-      game.moveCar(game.playerOneTrack,carLocation)
-      carLocation = game.carCurrentLocation(game.playerOneTrack)
-      game.checkWinner(carLocation, game.racetrackLength, game.player1)
-    }else if(e.charCode == 108){
-      carLocation = game.carCurrentLocation(game.playerTwoTrack)
-      game.moveCar(game.playerTwoTrack,carLocation)
-      carLocation = game.carCurrentLocation(game.playerTwoTrack)
-      game.checkWinner(carLocation, game.racetrackLength, game.player2)
+      racetrack.playerOneTrack.append("<td></td>")
+      racetrack.playerTwoTrack.append("<td></td>")
     }
   },
 
   carCurrentLocation: function(carTrack){
-    for(var i=0; i<game.racetrackLength; i++){
+    for(var i=0; i<racetrack.racetrackLength; i++){
       if(carTrack.children()[i].className === "active"){
         return i
       }
     }
   },
+},
 
+engine={
   moveCar: function(carTrack, carLocation){
     carTrack.children()[carLocation].className=""
     carTrack.children()[carLocation+1].className="active"
   },
+  listen: function(e){
+    if(e.charCode == 97){
+      racetrack.carLocation = racetrack.carCurrentLocation(racetrack.playerOneTrack)
+      engine.moveCar(racetrack.playerOneTrack,racetrack.carLocation)
+      racetrack.carLocation = racetrack.carCurrentLocation(racetrack.playerOneTrack)
+      game.checkWinner(players.player1)
+    }else if(e.charCode == 108){
+      racetrack.carLocation = racetrack.carCurrentLocation(racetrack.playerTwoTrack)
+      engine.moveCar(racetrack.playerTwoTrack,racetrack.carLocation)
+      racetrack.carLocation = racetrack.carCurrentLocation(racetrack.playerTwoTrack)
+      game.checkWinner(players.player2)
+    }
+  },
+},
 
-  checkWinner: function(carLocation, racetrackLength, player){
-    if(carLocation + 1 === racetrackLength){
-      console.log(player + " is the winner")
-      window.removeEventListener("keypress", game.jsRacerGame)
+game = {
+  resetGame: function(){
+    location.reload()
+  },
+
+  checkWinner: function(player){
+    if(racetrack.carLocation + 1 === racetrack.racetrackLength){
+      alert(player + " is the winner")
+      window.removeEventListener("keypress", engine.listen)
       game.resetGame()
     }
   },
+},
 
-
-}
-
-window.addEventListener("keypress", game.jsRacerGame)
+window.addEventListener("keypress", engine.listen)
 
 
 
